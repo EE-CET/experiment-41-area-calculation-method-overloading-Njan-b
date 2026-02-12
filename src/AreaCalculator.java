dd449e0b80c13cd86ad03d11bdb1b8e4084acbb1
Njan-b <dhrishyadasph2005@gmail.com>
2026-02-12 16:56:09 +0530

    initial commit

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,21 +1,45 @@
 import java.util.Scanner;
 
-public class AreaCalculator {
-    
-    // TODO: Define calculateArea(float radius) -> returns area of circle
-    // Use Math.PI or 3.14159f for pi
+class AreaCalculator {
 
-    // TODO: Define calculateArea(float length, float breadth) -> returns area of rectangle
+    // Area of Circle
+    float calculateArea(float radius) {
+        return (float)(Math.PI * radius * radius);
+    }
 
-    // TODO: Define calculateArea(double base, double height) -> returns area of triangle
+    // Area of Rectangle
+    float calculateArea(float length, float breadth) {
+        return length * breadth;
+    }
+
+    // Area of Triangle
+    double calculateArea(double base, double height) {
+        return 0.5 * base * height;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read input for Circle (radius)
-        // TODO: Read input for Rectangle (length, breadth)
-        // TODO: Read input for Triangle (base, height)
-        
-        // TODO: Call methods and print results formatted to 2 decimal places
-        // Hint: Use System.out.printf("%.2f\n", area);
+        AreaCalculator obj = new AreaCalculator();
+
+        // Input
+        float radius = sc.nextFloat();
+
+        float length = sc.nextFloat();
+        float breadth = sc.nextFloat();
+
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        // Calculations
+        float circleArea = obj.calculateArea(radius);
+        float rectangleArea = obj.calculateArea(length, breadth);
+        double triangleArea = obj.calculateArea(base, height);
+
+        // Output (2 decimal places)
+        System.out.printf("%.2f\n", circleArea);
+        System.out.printf("%.2f\n", rectangleArea);
+        System.out.printf("%.2f\n", triangleArea);
+
+        sc.close();
     }
 }
